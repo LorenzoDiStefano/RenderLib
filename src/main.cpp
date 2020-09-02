5f531fee6f0dd0c43c78625ce120c8378be0549d
Lorenzo Di Stefano <lorenzodistefano.k@gmail.com>
2020-09-02 18:58:29 +0200

    Include glfw

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <glm/vec2.hpp>
+#include <GLFW/glfw3.h>
 
 int main()
 {
@@ -7,5 +8,16 @@ int main()
 	glm::vec2 vector2_test(5, 6);
 	std::cout << "vector2_test x"<< vector2_test.x;
 
+	if (!glfwInit())
+	{
+		// Initialization failed
+		std::cout << "\nInitialization failed";
+	}
+
+	int monitors;
+	glfwGetMonitors(&monitors);
+	std::cout << "\nmonitors : " << monitors;
+
+	glfwTerminate();
 	return 0;
 }
