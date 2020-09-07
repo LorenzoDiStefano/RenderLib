f1e6d3bb4676d900d36b3380f82f09ca9611a166
Lorenzo Di Stefano <lorenzodistefano.k@gmail.com>
2020-09-07 15:17:45 +0200

    Include assimp

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <glm/vec2.hpp>
 #include <GLFW/glfw3.h>
+#include <assimp/color4.h>
 
 int main()
 {
 	
 	glm::vec2 vector2_test(5, 6);
 	std::cout << "vector2_test x"<< vector2_test.x;
-
+	aiColor4D color{1,2,3,4};
 	if (!glfwInit())
 	{
 		// Initialization failed
@@ -17,6 +18,7 @@ int main()
 	int monitors;
 	glfwGetMonitors(&monitors);
 	std::cout << "\nmonitors : " << monitors;
+	std::cout << "\ncolor : " << color.a;
 
 	glfwTerminate();
 	return 0;
